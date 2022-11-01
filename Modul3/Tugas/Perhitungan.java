0ee92e03b62aee8b54a0ad687ecf8fd3d3251ea7
Minsa111 <dragonriderrgt@gmail.com>
2022-11-02 00:25:47 +0700

    yay

---
+++ Modul3/Tugas/Perhitungan.java
@@ -0,0 +1,11 @@
+package Modul3.Tugas;
+
+public class Perhitungan {
+    block r = new block();
+    void perhitungan(){
+        int length= r.getLength();
+        int width = r.getWidth();
+        int height= r.getHeight();
+
+    }
+}
